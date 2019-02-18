39c7ef8069faed124d02d45f0b9c17b54e247ebf
Deryugin <deryugin.denis@gmail.com>
2019-02-18 10:42:09 +0300

    Update `at' alias: now convert to mp3 and remove temp files

--- fish_functions/at.fish
+++ fish_functions/at.fish
@@ -1,3 +1,9 @@
 function at
-    youtube-dl -i -f bestaudio --add-metadata $argv
+    youtube-dl -i -f bestaudio --add-metadata -o '%(title)s.%(ext)s' $argv
+    set fname (youtube-dl -i -f bestaudio $argv --get-filename -o '%(title)s.%(ext)s')
+    echo $fname
+    set fmp3 (youtube-dl $argv --get-filename -o '%(title)s.mp3')
+    echo $fmp3
+    ffmpeg -i $fname $fmp3
+    rm $fname
 end
