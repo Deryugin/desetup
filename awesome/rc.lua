9f45374582c9b95c7ccd68b853df57fce72ef863
Deryugin <deryugin.denis@gmail.com>
2019-04-06 21:26:55 +0300

    awesome: Fix hotkey collision

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -275,8 +275,8 @@ globalkeys = gears.table.join(
         end,
         {description = "focus previous by index", group = "client"}
     ),
-    awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
-              {description = "show main menu", group = "awesome"}),
+    --awful.key({ modkey,           }, "w", function () mymainmenu:show() end,
+    --          {description = "show main menu", group = "awesome"}),
 
     -- Layout manipulation
     awful.key({ modkey, "Shift"   }, "#44", function () awful.client.swap.byidx(  1)    end,
