284bd331bcd90d919005188ffbfd627fbbc39332
Deryugin <deryugin.denis@gmail.com>
2019-04-13 09:39:22 +0300

    Update at function

--- fish_functions/at.fish
+++ fish_functions/at.fish
@@ -1,9 +1,3 @@
 function at
-    youtube-dl -i -f bestaudio --add-metadata -o '%(title)s.%(ext)s' $argv
-    set fname (youtube-dl -i -f bestaudio $argv --get-filename -o '%(title)s.%(ext)s')
-    echo $fname
-    set fmp3 (youtube-dl $argv --get-filename -o '%(title)s.mp3')
-    echo $fmp3
-    ffmpeg -i $fname $fmp3
-    rm $fname
+	youtube-dl --extract-audio --audio-format mp3 $argv
 end
